e9e323a95fe97e8f4efc0871502fc89c5288e6d6
Edward Arechwa <e.arechwa@digitalcolliers.com>
2021-09-25 17:44:31 +0200

    initial

---
+++ src/components/header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header className="header">
+    <h1 className="header__title">
+      <span className="header__field">Report for</span>
+      Jeremy Robson
+    </h1>
+    <img src="/assets/avatar.png" alt="jeremy" className="header__image" />
+    <ul className="filters header__filters">
+      <li className="filters__item">
+        <button className="filters__button">Daily</button>
+      </li>
+      <li className="filters__item">
+        <button className="filters__button">Weekly</button>
+      </li>
+      <li className="filters__item">
+        <button className="filters__button">Monthly</button>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
---
+++ src/components/statistic/Statistic.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul className="statistic">
+    <li className="statistic__item">
+      <div className="statistic__body">
+        <header className="statistic__header">
+          <h3 className="statistic__title">Work</h3>
+          <button className="statistic__more">more</button>
+        </header>
+        <div className="statistic__count">
+          <p className="statistic__current">32hrs</p>
+          <p className="statistic__last">Last Week - 36hrs</p>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Statistic",
+});
+</script>
